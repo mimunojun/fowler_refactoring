289d9355cb4e1d6e6901774c54f072fec0b7d523
suzume <mimura.tsukuba@gmail.com>
2026-09-20 00:14:27 +0900

    thisAmount(変数のインライン化)

--- src/section-01/statement.ts
+++ src/section-01/statement.ts
@@ -28,15 +28,13 @@ export function statement(invoice: Invoice, plays: Play): string {
   }).format;
 
   for (let perf of invoice.performances) {
-    const thisAmount = amountFor(perf)
-
     // ボリューム特典のポイントを加算
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 喜劇医のときは10人につき、さらにポイントを加算
     if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
     //　注文の内訳を出力
-    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
