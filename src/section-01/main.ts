984f6d430563799194c8d898b8c566829d411c7b
suzume <mimura.tsukuba@gmail.com>
2026-09-19 17:26:19 +0900

    着手前 リファクタリング前の初期状態

---
+++ src/section-01/main.ts
@@ -0,0 +1,73 @@
+import { readFileSync } from "node:fs";
+
+const invoices: Invoice[] = JSON.parse(
+  readFileSync(new URL("./invoices.json", import.meta.url), "utf-8"),
+);
+const plays: Play = JSON.parse(
+  readFileSync(new URL("./plays.json", import.meta.url), "utf-8"),
+);
+
+for (const invoice of invoices) {
+  console.log(statement(invoice, plays));
+}
+
+type Invoice = {
+  customer: string;
+  performances: [{ playID: string, audience: number }];
+};
+
+type Play = {
+  [playID: string]: {
+    name: string;
+    type: string;
+  }
+};
+
+function statement(invoice: Invoice, plays: Play): string {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+
+    if (play == null) {
+      throw new Error(`unknown playID: ${perf.playID}`);
+    }
+
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+
+    // ボリューム特典のポイントを加算
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // 喜劇医のときは10人につき、さらにポイントを加算
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    //　注文の内訳を出力
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+    totalAmount += thisAmount;
+  }
+  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+  return result;
+}
